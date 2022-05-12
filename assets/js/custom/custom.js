5cd55dd71d6d6e317e8b68a547694ca455d3d268
mehed-hasan <mehedi.hasan@intelli.global>
2022-05-12 19:00:17 +0600

    Push

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -2,10 +2,8 @@
 
 // Code for preloader will be disappeared after all contents being loaded ----------------------------------------------------------*****
 $(window).on('load', function () {
-    $(".left_bar ").fadeOut(500); 
-    $(".left_bar img").fadeOut(200);
+    $("#preloader ").fadeOut(500); 
     $("#main").css({'overflow':'unset'}); 
-
 
 });
 // Code for preloader will be disappeared after all contents being loaded ######################################-------------------------- Ended 
