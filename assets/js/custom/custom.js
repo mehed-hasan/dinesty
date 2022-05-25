596b0f8b43f47276cace9860aae1e2dbf17b919d
mehed-hasan <mehedi.hasan@intelli.global>
2022-05-25 19:09:19 +0600

    2.2

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -22,3 +22,57 @@ $(window).on('load', function () {
         $(this).parent().parent().find(".p_price").text($price_val+".00");
     });
 
+
+
+    
+        // counter btn script
+        $initial_price = $(".quantity_price span").text();
+        $(".add_btn").click(function(){
+            $current_val = parseInt($(this).parent().parent().parent().find("input").val());
+            $update_val = $current_val + 1;
+            $(this).parent().parent().parent().find("input").val($update_val);
+
+          calculate_price($update_val);
+        });
+    
+        $(".remove_btn").click(function(){
+            $current_val = parseInt($(this).parent().parent().parent().find("input").val());
+
+            $update_val = $current_val - 1;
+            $(this).parent().parent().parent().find("input").val($update_val);
+    
+            if($update_val < 1){
+              $(this).parent().parent().parent().find("input").val(1);
+              $(".quantity_price span").text($initial_price+".00");
+
+            }else{
+              calculate_price($update_val);
+            }
+            
+        });
+
+        function calculate_price($updated_val){
+          
+          $initial_price = parseInt($initial_price);
+          $total_price = $initial_price * $updated_val;
+          $(".quantity_price span").text($total_price+".00");
+        }
+
+        // Cart Sliding functionalties
+
+        $(".cart_btn").click(function(){
+          $(".cart_body").addClass("cart_body_active ");
+          $("body").css({'overflow':'hidden'});
+
+
+        });
+
+   
+        $(".closer, .closer_btn").click(function(){
+          
+
+          $(".cart_body").removeClass("cart_body_active");
+          $("body").css({'overflow':'unset'});
+        })
+
+        // Cart Sliding functionalties
