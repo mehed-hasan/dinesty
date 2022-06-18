3f544349703d132c301ce44f150ff9cda13b5c39
mehed-hasan <mehedi.hasan@intelli.global>
2022-06-18 13:09:10 +0600

    Updated with feedback

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -1,9 +1,13 @@
 
 
+// Cookies modal 
+
+
 // Code for preloader will be disappeared after all contents being loaded ----------------------------------------------------------*****
 $(window).on('load', function () {
     $("#preloader ").fadeOut(500); 
     $("#main").css({'overflow':'unset'}); 
+    $(".cookies_modal").modal('show');
 
 });
 
